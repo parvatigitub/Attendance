a647d03eddbac827cc5a7afc06af38cbaebb3f1b
parvatigitub <yadavparvati679@gmail.com>
2025-07-22 11:38:10 +0530

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'shivam_attendnce_models'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/attendance_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@192.168.100.11:5432/attendance_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
     PROFILE_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'profiles')
