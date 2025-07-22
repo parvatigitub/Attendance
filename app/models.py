b47cc637d731e8a0c9271d5affc6362c396431cd
parvatigitub <yadavparvati679@gmail.com>
2025-07-22 16:39:22 +0530

    models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
-from app import db
+from . import db
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
