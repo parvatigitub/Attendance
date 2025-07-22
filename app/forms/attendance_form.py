12dcb349590c3326ed29f4d6c1ea7a11ddb2552b
parvatigitub <yadavparvati679@gmail.com>
2025-07-22 16:29:24 +0530

    attendance_form.py

--- app/forms/attendance_form.py
+++ app/forms/attendance_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import HiddenField
 from wtforms import BooleanField, IntegerField
+from wtforms.validators import NumberRange
 
 class AttendanceForm(FlaskForm):
     emp_id = HiddenField()  # Employee ID (hidden input in form)
