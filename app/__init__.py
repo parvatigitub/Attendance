e25f3523a265dd4d436bc115407de9d22fa3ff53
parvatigitub <yadavparvati679@gmail.com>
2025-07-22 15:31:03 +0530

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from config import Config
-from app.utils import create_app
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()
