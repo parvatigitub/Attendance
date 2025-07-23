30eeb23e8a1de8d11b01ebbb6e6c410c94b7a92b
npy11 <neerajp819168@gmail.com>
2025-07-24 00:14:41 +0530

    changes added

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     if os.environ.get("RENDER") == "true":
         SQLALCHEMY_DATABASE_URI = 'postgresql://root:CGIf9UBARjkPDGij5fLHzIN0Rl2Mmcac@dpg-d1vku6h5pdvs73e84ls0-a/attendance_db_38bl'
     else:
-        SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/attendance_db'
+        SQLALCHEMY_DATABASE_URI = "postgresql://root:CGIf9UBARjkPDGij5fLHzIN0Rl2Mmcac@dpg-d1vku6h5pdvs73e84ls0-a.oregon-postgres.render.com/attendance_db_38bl"
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
