0ea1fe2db0d252500abca64a23927d57ff4ac0d8
parvatigitub <yadavparvati679@gmail.com>
2025-07-23 18:54:37 +0530

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from app import app, db
+from app import app
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+
+if __name__ == '__main__':
     app.run(debug=True)
